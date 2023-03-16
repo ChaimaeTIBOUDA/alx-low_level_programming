5993470edc50f10f485137ee9adef15902042d4f
chaimae <chaimae@gmail.com>
2023-03-16 14:44:47 -0700

    adv

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <unisted.h>
+/**
+ * main - main function
+ * Return: this time is 1
+ */
+int main(void)
+{
+	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+	return (1);
+}
