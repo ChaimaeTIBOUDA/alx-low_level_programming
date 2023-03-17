bf90110740ba33a45310c39f525bd6cde9784c6e
chaimae <chaimae@gmail.com>
2023-03-17 08:36:57 -0700

    var

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - main function
+ * Return: always 0
+ */
+int main(void)
+{
+	char i;
+
+	for (i = 'a' ; i <= 'z' ; i++)
+	{
+		if (i == 'e' || i == 'q')
+			i++;
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
