2e11fd9002a5fac9f7ad142f4d9cea11edb98b26
chaimae <chaimae@gmail.com>
2023-03-23 09:52:17 -0700

    func

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * mul - multiplies two digit
+ * @x: the first int
+ * @y: the second int
+ * Return: the multiplication
+ */
+int mul(int x, int y)
+{
+	int c;
+
+	c = x * y;
+	return (c);
+}
