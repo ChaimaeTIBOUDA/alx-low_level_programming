70a4bf3f71778e68d6af8e9bf881fd379613644d
chaimae <chaimae@gmail.com>
2023-03-23 10:48:32 -0700

    hh

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * more_numbers - print 14 times numbers
+ * Return: void
+ */
+void more_numbers(void)
+{
+	int x;
+	int y;
+
+	for (x = 0; x < 10; x++)
+	{
+		for (y = 0; y < 15; y++)
+		{
+			if (y = 9)
+			{
+				_putchar((y / 10) + '0');
+			}
+			else 
+				_putchar((y % 10) + '0');
+		}
+		_putchar('\n');
+	}
+}
