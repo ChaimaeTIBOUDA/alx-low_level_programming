eb758e75f415fe6b3a36089ccd5d0ed7b1d886f9
ChaimaeTIBOUDA <125551991+ChaimaeTIBOUDA@users.noreply.github.com>
2023-04-27 20:42:44 +0000

    linked lists

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+/**
+ * add_node - add new node at the beginning of list_t
+ * @head: double pointer
+ * @str: the string to add
+ * Return: add of new node
+ */
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *n;
+	unsigned int l = 0;
+
+	while (str[l])
+		l++;
+	n = malloc(sizeof(list_t));
+	if (!n)
+		return (NULL);
+	n->str = strdup(str);
+	n->len = l;
+	n->next = (*head);
+	(*head) = n;
+	return(*head);
+}
