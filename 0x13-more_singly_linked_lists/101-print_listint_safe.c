ad7067a3bf6ae27d758c28fc3a1c6ccda312f9d2
ChaimaeTIBOUDA <125551991+ChaimaeTIBOUDA@users.noreply.github.com>
2023-05-03 21:53:00 +0000

    linked list

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -1,10 +1,42 @@
 #include "lists.h"
+size_t looped_listint_len(const listint_t *head);
 /**
  * print_listint_safe - print list
  * @head: pointer to the first node
  * Return: -0 or unique nodes
  */
 size_t print_listint_safe(const listint_t *head)
+{
+	size_t m;
+	size_t i = 0;
+
+	m = looped_listint_len(head);
+	if (m == 0)
+	{
+		for (; head != NULL; m++)
+		{
+			printf("[%p] %d\n", (void *)head, head->n);
+				head = head->next;
+		}
+	}
+	else
+	{
+		for (i = 0; i < m; i++)
+		{
+			printf("[%p] %d\n", (void *)head, head->n);
+				head = head->next;
+		}
+		printf("-> [%p] %d\n", (void *)head, head->n);
+	}
+	return (m);
+
+}
+/**
+ * looped_listint - how many unque node
+ * @head: pointer to the first node
+ * Return: -0 or nodes
+ */
+size_t looped_listint_len(const listint_t *head)
 {
 	const listint_t *a, *b;
 	size_t n = 1;
