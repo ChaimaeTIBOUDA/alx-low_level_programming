30c7956b43c8900447bbf0aef4c8c9aaac43d922
ChaimaeTIBOUDA <125551991+ChaimaeTIBOUDA@users.noreply.github.com>
2023-05-08 22:48:29 +0000

    file I/O

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * create_file - create file
+ * @filename: pointer to the name of file
+ * @text_content: pointer string to write
+ * Return: 1 or -1
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int f;
+	int w;
+	int i = 0;
+
+	if (filename == NULL)
+		return (-1);
+	if (text_content != NULL)
+	{
+		for (i = 0; text_content[i]; i++)
+			i++;
+	}
+	f = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+	w = write(f, text_content, i);
+	if (f == -1 || w == -1)
+		return (-1);
+	close(f);
+	return (1);
+}
