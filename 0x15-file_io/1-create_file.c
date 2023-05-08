b3adb5dabdf95b4e70570f29f4bde24b55f42549
ChaimaeTIBOUDA <125551991+ChaimaeTIBOUDA@users.noreply.github.com>
2023-05-08 23:36:35 +0000

    file i/o

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -13,11 +13,12 @@ int create_file(const char *filename, char *text_content)
 
 	if (filename == NULL)
 		return (-1);
-	f = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 	if (text_content != NULL)
-		text_content = "";
-	for (i = 0; text_content[i]; i++)
-		i++;
+	{
+		for (i = 0; text_content[i]; i++)
+			i++;
+	}
+	f = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 	w = write(f, text_content, i);
 	if (f == -1 || w == -1)
 		return (-1);
