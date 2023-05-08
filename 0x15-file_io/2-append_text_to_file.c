837e0bda4ee9d797a2ac757ac7aed8d82d5f172b
ChaimaeTIBOUDA <125551991+ChaimaeTIBOUDA@users.noreply.github.com>
2023-05-08 23:13:17 +0000

    file I/O

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -3,7 +3,7 @@
  * append_text_to_file - append text at the end odf file
  * @filename: pointer to the file
  * @text_content: string to add
- * Return: --1 or -1
+ * Return: 1 or -1
  */
 int append_text_to_file(const char *filename, char *text_content)
 {
