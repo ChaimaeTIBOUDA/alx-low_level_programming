62a36406f03c0753c353774c7c7c9e783a732b7c
ChaimaeTIBOUDA <125551991+ChaimaeTIBOUDA@users.noreply.github.com>
2023-05-25 22:30:04 +0000

    file I/O

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -29,8 +29,7 @@ void close_file(int fd)
 	cl = close(fd);
 	if (cl == -1)
 	{
-		dprintf(STDERR_FILENO,
-				"Error; Can't close fd %d\n", fd);
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd);
 		exit(100);
 	}
 }
@@ -42,40 +41,39 @@ void close_file(int fd)
  */
 int main(int argc, char *argv[])
 {
-	int f, t, r, w;
+	int fr, t, r, wr;
 	char *B;
 
 	if (argc != 3)
 	{
-		dprintf(STDERR_FILENO,
-				"Usage : cp file_from file_to\n");
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 		exit(97);
 	}
 	B = create_buffer(argv[2]);
-	f = open(argv[1], O_RDONLY);
-	r = read(f, B, 1024);
+	fr = open(argv[1], O_RDONLY);
+	r = read(fr, B, 1024);
 	t = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
 	do {
-		if (f == -1 || r == -1)
+		if (fr == -1 || r == -1)
 		{
 			dprintf(STDERR_FILENO,
 					"Error: Can't read from file %s\n", argv[1]);
-					free(B);
-					exit(98);
+			free(B);
+			exit(98);
 		}
-		w = write(t, B, r);
-		if (t == -1 || w == -1)
+		wr = write(t, B, r);
+		if (t == -1 || wr == -1)
 		{
 			dprintf(STDERR_FILENO,
 					"Error: Can't write to %s\n", argv[2]);
 			free(B);
 			exit(99);
 		}
-		r = read(f, B, 1024);
+		r = read(fr, B, 1024);
 		t = open(argv[2], O_WRONLY | O_APPEND);
 	} while (r > 0);
 	free(B);
-	close_file(f);
+	close_file(fr);
 	close_file(t);
 	return (0);
 }
