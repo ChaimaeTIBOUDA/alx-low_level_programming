0f36bc073deca984f0474f8f6b5c16b15921bbfc
ChaimaeTIBOUDA <125551991+ChaimaeTIBOUDA@users.noreply.github.com>
2023-05-25 22:02:58 +0000

    file I/O

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -1,27 +1,25 @@
 #include "main.h"
 /**
- * append_text_to_file - append text at the end odf file
- * @filename: pointer to the file
+ * append_text_to_file - append text to the end of file
+ * @filename: pointer to the name of file
  * @text_content: string to add
  * Return: 1 or -1
  */
 int append_text_to_file(const char *filename, char *text_content)
 {
-	int op;
-	int w;
-	int i;
+	int op, wr, l = 0;
 
 	if (filename == NULL)
 		return (-1);
 	if (text_content != NULL)
 	{
-		for (i = 0; text_content[i]; i++)
-			i++;
+		for (l = 0; text_content[l];)
+			l++;
 	}
-		op = open(filename, O_WRONLY | O_APPEND);
-		w = write(op, text_content, i);
-		if (op == -1 || w == -1)
-			return (-1);
-		close(op);
-		return (1);
+	op = open(filename, O_WRONLY | O_APPEND);
+	wr = write(op, text_content, l);
+	if (op == -1 || wr == -1)
+		return (-1);
+	close (op);
+	return (1);
 }
