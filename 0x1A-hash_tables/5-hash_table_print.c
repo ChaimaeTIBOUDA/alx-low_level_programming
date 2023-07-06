b54b56b39a62cd14a9f67b8520f64e2ce225889c
ChaimaeTIBOUDA <125551991+ChaimaeTIBOUDA@users.noreply.github.com>
2023-07-06 20:48:20 +0000

    hash_table

--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -9,14 +9,14 @@ void hash_table_print(const hash_table_t *ht)
 	unsigned long int i, c = 0;
 	hash_node_t *node;
 
-	if (t == NULL)
+	if (ht == NULL)
 		return;
 	printf("{");
-	for (i = 0; i < t->size; i++)
+	for (i = 0; i < ht->size; i++)
 	{
-		if (t->array[i] != NULL)
+		if (ht->array[i] != NULL)
 		{
-			node = t->array[i];
+			node = ht->array[i];
 			while (node != NULL)
 			{
 				if (c > 0)
